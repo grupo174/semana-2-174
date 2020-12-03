518ff19fe3d929a186cf22245adb3ca6de18b0f3
YanedS <yavisanchez@utp.edu.co>
2020-12-03 18:57:47 -0500

    Se agrego class de imagen

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -9,6 +9,35 @@
         name: "TeamCard",
         props: ['member']
     }
+    team: [{
+    codigo: 1,
+    Nombre: 'Luis Codo',
+    Descripcion: 'texto.',
+    Rol: 'Desarrollador backend',
+    }, {
+    codigo: 2,
+    Nombre: 'Orlando Palacios',
+    Descripcion: 'texto.',
+    Rol: 'Analista de Software',
+   
+    }, {
+    codigo: 3,
+    Nombre: 'Juan Esteban Quintero',
+    Descripcion: 'texto.',
+    Rol: 'Desarrollador frontend',
+   
+    }, {
+    codigo: 4,
+    Nombre: 'Nallive Trujillo',
+    Descripcion: 'texto.',
+    Rol: 'Arquitecto de software',
+   
+    }, {
+    codigo: 5,
+    Nombre: 'Yaned Sánchez',
+    Descripcion: 'texto.',
+    Rol: 'Tester'
+    }]
 </script>
 
 <style scoped>
