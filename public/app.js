dd2f766eb61c93786a4d5c424c1223a5aaf7f814
YanedS <yavisanchez@utp.edu.co>
2020-12-03 17:28:11 -0500

    Conexión al Repositorio Sección 4

---
+++ public/app.js
@@ -0,0 +1,122 @@
+team: [{
+    codigo: 1,
+    Nombre: 'Luis Codo',
+    Descripcion: 'texto.',
+    Rol: 'Desarrollador backend',
+    }, {
+    codigo: 2,
+    Nombre: 'Orlando Palacios',
+    Descripcion: 'texto.',
+    Rol: 'Analista de Software',
+   
+    }, {
+    codigo: 3,
+    Nombre: 'Juan Esteban Quintero',
+    Descripcion: 'texto.',
+    Rol: 'Desarrollador frontend',
+   
+    }, {
+    codigo: 4,
+    Nombre: 'Nallive Trujillo',
+    Descripcion: 'texto.',
+    Rol: 'Arquitecto de software',
+   
+    }, {
+    codigo: 5,
+    Nombre: 'Yaned Sánchez',
+    Descripcion: 'texto.',
+    Rol: 'Tester'
+    }]
+
+
+    // if "codigo" %2 == 0 {
+
+    // }
+   
+
+
+
+
+
+// new Vue({
+//     el:#app,
+//     template: `<div v-bind: style="estilos" v-on: click="log()"`,
+//     data: {
+//         titulo: 'Rectangulo',
+//         largo: 100,
+//         alto: 40
+//     },
+//     computed: {
+//         area: function (){
+//             return this.largo * this.alto;
+//         },
+//         estilos: function(){
+//             return{
+//                 widht: this.largo + 'px',
+//                 height: this.alto + 'px'
+//             }
+//         }
+//     }
+//     methods: {
+//         log: function(){
+//             console.log('Soy un rectangulo con un area de ' + this.area + 'pixeles');
+//         }
+//     }
+//     created: function(){
+//         console.log ('Se crea la instancia: todavía no se ha reemplazado "#app" por el template de estainstancia');
+//     },
+//     mounted: function(){
+//         console.log('ya se ha renderizado el template en el DOM');
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var app = new Vue ({
+//     el: '#app',
+//     data: {
+//         message: 'Hola Vue!',
+//         title: 'Tareas con Vue g-día',
+//         sesion: 5,
+//         profesores: {}
+//             'nombre': 'Luis Codo',
+//             nuevaTarea:''
+//         }
+//     },
+//     methods: {
+//         agregarTarea (){
+//             console.log (this.nuevaTarea)
+//         }
+//     }
+//     computed: {
+
+//     },
+//     beforeCreate(){
+
+//     },
+//     created(){
+
+//     },
+// })
