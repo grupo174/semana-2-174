484cb87e089a81cd1332235fdd3a0411c989bde9
YanedS <yavisanchez@utp.edu.co>
2020-12-03 21:44:08 -0500

    Actualizacion archivos html, js y css

--- public/app.js
+++ public/app.js
@@ -1,122 +1,41 @@
 var app = new Vue({
-    el:'#app',
+    el: "#app",
     data: {
-        titulo: 'TeamCard';
-        team: [{
-            codigo: 1,
-            nombre: 'Luis Codo',
-            descripcion: 'Edad: 37. Profesión: Profesor de Informática Hobbies: Videojuegos (Valorant, Age of Empires, World of Warcraft), cocinar, lecturas, mangas y animés, películas.',
-            rol: 'Desarrollador backend',
-            image: ''
+        titulo: 'Ejercicio',
+        team: [
+            {
+                codigo: 1,
+                nombre: 'Luis Codo',
+                descripcion: 'Edad: 37. Profesión: Profesor de Informática Hobbies: Videojuegos (Valorant, Age of Empires, World of Warcraft), cocinar, lecturas, mangas y animés, películas.',
+                rol: 'Desarrollador backend',
+                // image: ''
             }, {
-            codigo: 2,
-            nombre: 'Orlando Palacios',
-            escripcion: 'texto.',
-            Rol: 'Analista de Software',
-            image: ''
+                codigo: 2,
+                nombre: 'Orlando Palacios',
+                descripcion: 'Edad: 29. Profesión: Ingeniero de Petroleos. Hobbies: Tecnología, lectura y temas paranormales.',
+                rol: 'Analista de Software',
+                // image: ''
            
             }, {
-            codigo: 3,
-            nombre: 'Juan Esteban Quintero',
-            descripcion: 'texto.',
-            rol: 'Desarrollador frontend',
-            image: ''
+                codigo: 3,
+                nombre: 'Juan Esteban Quintero',
+                descripcion: 'Edad: 26. Profesión: Ingeniero de Sistemas y Computación. Hobbies: Ciclismo, Videojuegos Lectura (manga, novelas y comics).',
+                rol: 'Desarrollador frontend',
+                // image: ''
            
             }, {
-            codigo: 4,
-            nombre: 'Nallive Trujillo',
-            descripcion: 'texto.',
-            rol: 'Arquitecto de software',
-            image: ''
+                codigo: 4,
+                nombre: 'Nallive Trujillo',
+                descripcion: 'Edad: 30. Profesión: Actualmente estudia un curso de programacion por el convenio Mision TIC-UTP 2022. Hobbies: Leer libros, blogs, revistas, etc.',
+                rol: 'Arquitecto de software',
+                // image: ''
            
             }, {
-            codigo: 5,
-            nombre: 'Yaned Sánchez',
-            descripcion: 'texto.',
-            rol: 'Tester'
-            image: ''
+                codigo: 5,
+                nombre: 'Yaned Sánchez',
+                descripcion: 'Edad: 27. Profesión: Licenciada en comunicación e informática educativa. Hobbies: Escuchar música, ver películas y series, dibujar.',
+                rol: 'Tester',
+                // image: ''
             }]
-
     }
-
-    
-//     template: `<div v-bind: style="estilos" v-on: click="log()"`,
-//     data: {
-//         titulo: 'Rectangulo',
-//         largo: 100,
-//         alto: 40
-//     },
-//     computed: {
-//         area: function (){
-//             return this.largo * this.alto;
-//         },
-//         estilos: function(){
-//             return{
-//                 widht: this.largo + 'px',
-//                 height: this.alto + 'px'
-//             }
-//         }
-//     }
-//     methods: {
-//         log: function(){
-//             console.log('Soy un rectangulo con un area de ' + this.area + 'pixeles');
-//         }
-//     }
-//     created: function(){
-//         console.log ('Se crea la instancia: todavía no se ha reemplazado "#app" por el template de estainstancia');
-//     },
-//     mounted: function(){
-//         console.log('ya se ha renderizado el template en el DOM');
-//     }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var app = new Vue ({
-//     el: '#app',
-//     data: {
-//         message: 'Hola Vue!',
-//         title: 'Tareas con Vue g-día',
-//         sesion: 5,
-//         profesores: {}
-//             'nombre': 'Luis Codo',
-//             nuevaTarea:''
-//         }
-//     },
-//     methods: {
-//         agregarTarea (){
-//             console.log (this.nuevaTarea)
-//         }
-//     }
-//     computed: {
-
-//     },
-//     beforeCreate(){
-
-//     },
-//     created(){
-
-//     },
-// })
+});
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-
-    </div>
+     <div id="app">
+       <img :src="require('./assets/logo.png')" />
+     </div>
 </template>
 
 <script>
@@ -9,37 +9,17 @@
         name: "TeamCard",
         props: ['member']
     }
-    team: [{
-    codigo: 1,
-    Nombre: 'Luis Codo',
-    Descripcion: 'texto.',
-    Rol: 'Desarrollador backend',
-    }, {
-    codigo: 2,
-    Nombre: 'Orlando Palacios',
-    Descripcion: 'texto.',
-    Rol: 'Analista de Software',
-   
-    }, {
-    codigo: 3,
-    Nombre: 'Juan Esteban Quintero',
-    Descripcion: 'texto.',
-    Rol: 'Desarrollador frontend',
-   
-    }, {
-    codigo: 4,
-    Nombre: 'Nallive Trujillo',
-    Descripcion: 'texto.',
-    Rol: 'Arquitecto de software',
-   
-    }, {
-    codigo: 5,
-    Nombre: 'Yaned Sánchez',
-    Descripcion: 'texto.',
-    Rol: 'Tester'
-    }]
+    
 </script>
 
 <style scoped>
 
+</style>
+
+
+export default {
+}
+</script>
+
+<style lang="scss">
 </style>
